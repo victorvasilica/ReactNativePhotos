c2d2ef6799ba291447262044de34620dea2136d6
Victor Vasilica <vasilica.victor@gmail.com>
2018-04-05 17:54:23 +0300

    Add '_getPhotos(albumId)' method to fetch all photos by given album id
    The method is used when the component is mounted via
    'componentDidMount()' method. After the data is fetched the state is
    updated with the new data.

--- Photos.js
+++ Photos.js
@@ -2,6 +2,31 @@ import React from 'react';
 import { Text, View, ActivityIndicator, FlatList } from 'react-native';
 
 export default class Photos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isLoading: true}
+  }
+
+  componentDidMount() {
+    this._getPhotos(this.props.album.id)
+  }
+
+  _getPhotos(albumId) {
+    const url = 'https://jsonplaceholder.typicode.com/albums/' + albumId + '/photos';
+
+    return fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
   render() {
     return (
         <View></View>
