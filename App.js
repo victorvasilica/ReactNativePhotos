35937f0a36220c0d02abdcf8e6d5c0ba83a0ba15
Victor Vasilica <vasilica.victor@gmail.com>
2018-04-05 12:31:18 +0300

    Add 'Albums' component as the root of the navigatator

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { NavigationIOS } from 'react-native';
+import { StyleSheet, NavigatorIOS } from 'react-native';
 import Albums from './Albums';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: '',
+          component: Albums
+        }}/>
     );
   }
 }
@@ -17,8 +18,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
