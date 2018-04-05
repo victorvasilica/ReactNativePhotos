976fc0e41686127e3db3a2516507e593cb1960f5
Victor Vasilica <vasilica.victor@gmail.com>
2018-04-05 13:21:11 +0300

    Add '_getAlbums()' method to fetch data from jsonplaceholder APi
    The method is used when the component is mounted via
    'componentDidMount()' method. Updateding the state triggers an UI
    update that uses the new data to display it in the list.
    
    Closes enhancement #1

--- Albums.js
+++ Albums.js
@@ -4,8 +4,27 @@ import { Text, View, ActivityIndicator, FlatList } from 'react-native';
 export default class Albums extends React.Component {
   constructor(props){
     super(props);
-    this.state = { isLoading: true}
+    this.state = {isLoading: true}
   }
+
+  _getAlbums() {
+    return fetch('https://jsonplaceholder.typicode.com/albums')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  }
+
+  componentDidMount() {
+    this._getAlbums();
+  }
+
   render(){
 
     if(this.state.isLoading){
@@ -20,7 +39,7 @@ export default class Albums extends React.Component {
       <View style={{flex: 1, paddingTop: 20}}>
         <FlatList
           data={this.state.dataSource}
-          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          renderItem={({item}) => <Text>{item.title}</Text>}
           keyExtractor={(item, index) => index}
         />
       </View>
