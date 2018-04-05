1e02ecb97eb5a1afddf33514e997f53bc6ebcdec
Victor Vasilica <vasilica.victor@gmail.com>
2018-04-05 12:54:40 +0300

    Added missing import 'ActivityIndicator' and 'FlatList'
    'ActivityIndicator' is needed to inform the user that the data is
    loading. 'FlatList' is needed to show the list of albums fetched from
    the API.

--- Albums.js
+++ Albums.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, ActivityIndicator, FlatList } from 'react-native';
 
 export default class Albums extends React.Component {
-  render() {
-    return (
-      <View>
-          <Text>Albums page </Text>
+  constructor(props){
+    super(props);
+    this.state = { isLoading: true}
+  }
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, justifyContent: 'center'}}>
+          <ActivityIndicator color='#0000ff'/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop: 20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={(item, index) => index}
+        />
       </View>
     );
-  };
+  }
 }
