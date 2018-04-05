1c9b3876ee73aa0db4f3760a83a6e7c489c6d87a
Victor Vasilica <vasilica.victor@gmail.com>
2018-04-05 20:37:23 +0300

    Display image in full screen maintaining aspect ratio

--- PhotoDetails.js
+++ PhotoDetails.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 
 export default class PhotoDetails extends React.Component {
   render() {
+    const title = this.props.photo.title;
+    const photoUrl = this.props.photo.url;
+
     return (
-      <View></View>
+      <View style={styles.container}>
+        <Image style={styles.image} source={{ uri: photoUrl}} />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  image: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    resizeMode: 'contain'
+  },
+});
