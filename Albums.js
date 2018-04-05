d9387c2421306e442c7738bf98662d1cb256f1b3
Victor Vasilica <vasilica.victor@gmail.com>
2018-04-05 11:56:50 +0300

    Add 'Albums.js' file to separate albums related code
    Created a separate file, 'Albums.js', to store the 'Albums' React
    Component and later additional styling code and items for displaying
    the albums.

---
+++ Albums.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default class Albums extends React.Component {
+  render() {
+    return (
+      <View>
+          <Text>Albums page </Text>
+      </View>
+    );
+  };
+}
