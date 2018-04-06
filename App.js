93dc08ae28a66cac5be144d056be02bab6132a39
Victor Vasilica <vasilica.victor@gmail.com>
2018-04-06 19:05:18 +0300

    Add reducer actions needed to update the state

--- App.js
+++ App.js
@@ -4,12 +4,49 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import Albums from './Albums';
 
-const fetcher = (state = [], action) => {
+const defaultState = {
+  albums: {
+    data: [],
+    isLoading: false
+  },
+  photos: {
+    data: [],
+    isLoading: false
+  }
+}
+
+const fetcher = (state = { defaultState }, action) => {
   switch (action.type) {
     case 'FETCH_ALBUMS':
-      return action.albums;
+      return {
+        ...state,
+        albums: {
+          isLoading: true
+        }
+      };
+    case 'UPDATE_ALBUMS':
+      return {
+        ...state,
+        albums: {
+          data: action.albums,
+          isLoading: false
+        }
+      };
     case 'FETCH_PHOTOS':
-      return action.photos;
+      return {
+        ...state,
+        photos: {
+          isLoading: true
+        }
+      };
+    case 'UPDATE_PHOTOS':
+      return {
+        ...state,
+        photos: {
+          data: action.photos,
+          isLoading: false
+        }
+      };
     default:
       return state;
   }
