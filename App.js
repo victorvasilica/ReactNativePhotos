c8619fef712858d10b24e9f3de753ff69ad4c204
Victor Vasilica <vasilica.victor@gmail.com>
2018-04-06 16:31:23 +0300

    Add Redux imports to create a store

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import { StyleSheet, NavigatorIOS } from 'react-native';
+import { createStore } from 'redux';
 import Albums from './Albums';
+
+const fetcher = (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_ALBUMS':
+      return action.albums;
+    case 'FETCH_PHOTOS':
+      return action.photos;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(fetcher);
 
 export default class App extends React.Component {
   render() {
